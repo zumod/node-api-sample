c587b46825c711d56890ef51e4038226a1ff074d
Sumod Sivadas <contactsumod@gmail.com>
2020-11-13 20:11:02 +0530

    Initial commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Sumod  ',
+    email: 'sumod@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Neha',
+    email: 'neha@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Akhil',
+    email: 'akhil@gmail.com',
+    status: 'active'
+  }
+];
+
+module.exports = members;
